3871b42f50f8da0cecb01e0b355338b4215c1ad7
imran4u633662 <iali00470@gmail.com>
2024-10-12 16:55:22 +0500

    first Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function addValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteChar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(display.value);
+        display.value = result;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
